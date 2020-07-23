4dfca3f68fb333f5654c02f6a107fa9b7e5dfc3b
Chase-42 <chaseofthecollins@gmail.com>
2020-07-22 20:00:04 -0600

    searchbookform

--- src/components/pages/NewBookPage.jsx
+++ src/components/pages/NewBookPage.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Segment } from 'semantic-ui-react';
+import SearchBookForm from '../forms/SearchBookForm';
 
 class NewBookPage extends Component {
   state = {
     book: null,
   };
   render() {
-    return <div></div>;
+    return (
+      <Segment>
+        <h1>Add new book to your collection</h1>
+        <SearchBookForm />
+      </Segment>
+    );
   }
 }
 
